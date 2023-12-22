61ba3fb74375233b70db89d943a98adae10bc548
David <110460234+DavidCobb606@users.noreply.github.com>
2023-12-22 07:33:03 +0000

    formatting and typo corrected

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 //builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("WeatherServerDatabase"));
